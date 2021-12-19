54f30b3a067c723a6a7257eeb2c157ea51aa0bb3
lixiaoyu9 <lixiaoyu9@vipkid.com.cn>
2021-12-19 15:16:08 +0800

    update

--- ak47/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-exports.handler = (req, resp, context) => {
-    resp.setHeader('Content-Type', 'text/html; charset=utf-8');
-    resp.setHeader('Cache-Control', 'max-age=86400');
-    const html = fs.readFileSync('./index.html', 'utf8');
-    resp.send(html);
-}
